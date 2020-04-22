cdba62b5cf60c3376170d717c646180c43337dbb
Landon <norogoth@gmail.com>
2020-04-22 14:40:24 -0700

    Committing the basic files.

---
+++ C867_Project.cpp
@@ -0,0 +1,10 @@
+// C867_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Let's see if we can get this fucking shit to work. FUCK.\n";
+}
+
